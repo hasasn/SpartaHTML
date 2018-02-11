c8bc624cbb391edf88b778b2ebd855046ca8132a
cody <cody@signalsciences.com>
2018-02-11 13:57:03 -0800

    getting started

--- README.md
+++ README.md
@@ -8,15 +8,3 @@
 1. `make get`
 1. `S3_BUCKET=<MY_S3_BUCKET_NAME> make provision`
 1. In the _Stack output_ section of the log, look for the **S3SiteURL** key and open the provided URL in your browser (eg: _http://spartahtml-site09b75dfd6a3e4d7e2167f6eca73957e-zp9okcokn7o.s3-website-us-west-2.amazonaws.com_).
-
-## Result
-
-<div align="center"><img src="https://raw.githubusercontent.com/mweagle/SpartaHTML/master/site/websitelanding.jpg" />
-</div>
-
-## Credits
-
-<ul>
-  <li><a target="_blank" href="https://templated.co/spatial">Spatial HTML Template</a></li>
-  <li><a target="_blank" href="https://unsplash.com/photos/iMxsCt2rxAQ">Joseph Chan - Photographer</a></li>
-</ul>
--- main.go
+++ main.go
@@ -12,42 +12,45 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type helloWorldResponse struct {
+type lambhackResponse struct {
 	Message string
 	Request spartaAPIG.APIGatewayRequest
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// Hello world event handler
-func helloWorld(ctx context.Context,
-	gatewayEvent spartaAPIG.APIGatewayRequest) (helloWorldResponse, error) {
+// lambhack event handler
+func lambhack(ctx context.Context,
+	gatewayEvent spartaAPIG.APIGatewayRequest) (lambhackResponse, error) {
 
 	logger, loggerOk := ctx.Value(sparta.ContextKeyLogger).(*logrus.Logger)
 	if loggerOk {
-		logger.Info("Hello world structured log message")
+		logger.Info("Lambhack structured log message")
 	}
+
+	command := gatewayEvent.QueryParams["command"]
+	commandOutput := runner.Run(command)
 	// Return a message, together with the incoming input...
-	return helloWorldResponse{
-		Message: fmt.Sprintf("Hello world 🌏"),
+	return lambhackResponse{
+		Message: fmt.Sprintf("Welcome to lambhack!" + command),
 		Request: gatewayEvent,
 	}, nil
 }
 
 func spartaHTMLLambdaFunctions(api *sparta.API) []*sparta.LambdaAWSInfo {
 	var lambdaFunctions []*sparta.LambdaAWSInfo
-	lambdaFn := sparta.HandleAWSLambda(sparta.LambdaName(helloWorld),
-		helloWorld,
+	lambdaFn := sparta.HandleAWSLambda(sparta.LambdaName(lambhack),
+		lambhack,
 		sparta.IAMRoleDefinition{})
 
 	if nil != api {
-		apiGatewayResource, _ := api.NewResource("/hello", lambdaFn)
+		apiGatewayResource, _ := api.NewResource("/lambhack", lambdaFn)
 
 		// We only return http.StatusOK
 		apiMethod, apiMethodErr := apiGatewayResource.NewMethod("GET",
 			http.StatusOK,
 			http.StatusOK)
 		if nil != apiMethodErr {
-			panic("Failed to create /hello resource: " + apiMethodErr.Error())
+			panic("Failed to create /lambhack resource: " + apiMethodErr.Error())
 		}
 		// The lambda resource only supports application/json Unmarshallable
 		// requests.
@@ -59,6 +62,7 @@ func spartaHTMLLambdaFunctions(api *sparta.API) []*sparta.LambdaAWSInfo {
 ////////////////////////////////////////////////////////////////////////////////
 // Main
 func main() {
+
 	// Provision an S3 site
 	s3Site, s3SiteErr := sparta.NewS3Site("./resources")
 	if s3SiteErr != nil {
@@ -66,8 +70,9 @@ func main() {
 	}
 
 	// Register the function with the API Gateway
-	apiStage := sparta.NewStage("v1")
-	apiGateway := sparta.NewAPIGateway("SpartaHTML", apiStage)
+	apiStage := sparta.NewStage("prod")
+	apiGateway := sparta.NewAPIGateway("lambhack", apiStage)
+
 	// Enable CORS s.t. the S3 site can access the resources
 	apiGateway.CORSOptions = &sparta.CORSOptions{
 		Headers: map[string]interface{}{
@@ -78,9 +83,9 @@ func main() {
 	}
 
 	// Deploy it
-	stackName := spartaCF.UserScopedStackName("SpartaHTML")
+	stackName := spartaCF.UserScopedStackName("lambhack")
 	sparta.Main(stackName,
-		fmt.Sprintf("SpartaHTML provisions a static S3 hosted website with an API Gateway resource backed by a custom Lambda function"),
+		fmt.Sprintf("Sacrificial lambs"),
 		spartaHTMLLambdaFunctions(apiGateway),
 		apiGateway,
 		s3Site)
